344b42a332f420417f3b766b70b3e0d15cbe9916
bharat <joshibharat3010@gmail.com>
2025-05-24 09:45:40 +0530

    made changes in enqueue and dequeue operaations

--- code with harry/Chapter 12 Queue/queueEnqueAndDeque.c
+++ code with harry/Chapter 12 Queue/queueEnqueAndDeque.c
@@ -10,16 +10,16 @@ typedef struct queue
 }Queue;
 
 int isFull(Queue* q1){
-    if(q1->back == q1->size-1 && q1->front == -1){
+    if(q1->back == q1->size-1){
         return 1;
     }return 0;
 }
 int isEmpty(Queue* q1){
-    if(q1->back==-1 && q1->front == -1){
+    if(q1->back==q1->front){
         return 1;
     }return 0;
 }
-void enque(Queue* q1,int value){
+void enqueue(Queue* q1,int value){
     if(!isFull(q1)){
         q1->back++;
         q1->arr[q1->back] = value;
@@ -27,15 +27,16 @@ void enque(Queue* q1,int value){
         printf("enqueue operation failed \n");
     }
 }
-int deque(Queue* q1){
+int dequeue(Queue* q1){
     if(!isEmpty(q1)){
         q1->front++;
         return q1->arr[q1->front];
     }else{
         printf("dequeue operation failed \n");
+        return -1;
     }
 }
-void peek(Queue * q1){
+void display(Queue * q1){
     for(int i = q1->front+1; i<= q1->back; i++){
         printf("%d ",q1->arr[i]);
     }
@@ -49,25 +50,25 @@ int main()
     q1->front =-1;
     q1->size =10;
     q1->arr = (int *)malloc(sizeof(int)*q1->size);
-    enque(q1, 72);
-    enque(q1, 56);
-    enque(q1, 45);
-    enque(q1, 90);
-    enque(q1, 40);
-    enque(q1, 36);
-    enque(q1, 96);
-    enque(q1, 12);
-    enque(q1, 72);
-    enque(q1, 88);
-    enque(q1, 98);//queue is alredy full can't add it
-    peek(q1);
-    printf("let remove something : %d \n", deque(q1));
-    peek(q1);
-    enque(q1, 420);
-    peek(q1);
-    printf("let remove something : %d \n", deque(q1));
-    peek(q1);
-    enque(q1, 780);
-    peek(q1);
+    enqueue(q1, 72);
+    enqueue(q1, 56);
+    enqueue(q1, 45);
+    enqueue(q1, 90);
+    enqueue(q1, 40);
+    enqueue(q1, 36);
+    enqueue(q1, 96);
+    enqueue(q1, 12);
+    enqueue(q1, 72);
+    enqueue(q1, 88);
+    enqueue(q1, 98);//queue is alredy full can't add it
+    display(q1);
+    printf("let remove something : %d \n", dequeue(q1));
+    display(q1);
+    enqueue(q1, 420);
+    display(q1);
+    printf("let remove something : %d \n", dequeue(q1));
+    display(q1);
+    enqueue(q1, 780);//no matter we dequeueue or not once the queue is full it never accept the elements ever again
+    display(q1);
     return 0;
 }
